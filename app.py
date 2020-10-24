97ca8a5184ae2df027f51884496827d7b234865c
Rajvir Dua <rajd@Rajvirs-Mac-mini.local>
2020-10-24 12:02:43 -0700

    First commit

---
+++ app.py
@@ -0,0 +1,10 @@
+## Run this
+import speech_recognition as sr
+from speech_to_text import get_text
+
+
+def main():
+    get_text()
+    
+if __name__ == "__main__":
+    main()
---
+++ speech_to_text.py
@@ -0,0 +1,27 @@
+## Util file to convert speech input into text output
+
+# import the module 
+import speech_recognition as sr
+
+
+
+def get_text():# create the recognizer 
+    r = sr.Recognizer()
+
+    # define the microphone 
+    mic = sr.Microphone(device_index=0) 
+
+    # record your speech 
+    with mic as source: 
+        audio = r.listen(source) 
+
+    # speech recognition 
+    result = r.recognize_google(audio)
+
+    # export the result 
+    with open('text\my_result.txt',mode ='w') as file: 
+        file.write("Recognized text:") 
+        file.write("\n") 
+        file.write(result) 
+
+    print("Exporting process completed!")
